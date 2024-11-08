214188ff22f9bdcc95dadeed105c62cbc084c2d9
Patrick Hentschel <patrick_hentschel@outlook.de>
2024-11-08 22:47:30 +0100

    clean up

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,9 +4,10 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
-import * as $api_joke from "./routes/api/joke.ts";
-import * as $greet_name_ from "./routes/greet/[name].tsx";
+import * as $api_openfoodfacts from "./routes/api/openfoodfacts.tsx";
 import * as $index from "./routes/index.tsx";
+import * as $utils_interfaces_ProductInfo from "./routes/utils/interfaces/ProductInfo.tsx";
+import * as $utils_interfaces_ProductNutrients from "./routes/utils/interfaces/ProductNutrients.tsx";
 import * as $FPE_Calculator from "./islands/FPE-Calculator.tsx";
 import type { Manifest } from "$fresh/server.ts";
 
@@ -14,9 +15,11 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
-    "./routes/api/joke.ts": $api_joke,
-    "./routes/greet/[name].tsx": $greet_name_,
+    "./routes/api/openfoodfacts.tsx": $api_openfoodfacts,
     "./routes/index.tsx": $index,
+    "./routes/utils/interfaces/ProductInfo.tsx": $utils_interfaces_ProductInfo,
+    "./routes/utils/interfaces/ProductNutrients.tsx":
+      $utils_interfaces_ProductNutrients,
   },
   islands: {
     "./islands/FPE-Calculator.tsx": $FPE_Calculator,
